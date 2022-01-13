fc712ebc4eae560e449854c12bcac88e447bfb12
aniketkardule <82253148+aniketkardule@users.noreply.github.com>
2022-01-13 21:56:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="styles.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" type="text/css">
-
+        <script src="https://use.fontawesome.com/5907deaed2.js"></script>
+        
+        <link rel="stylesheet" href="font-awesome-4.7.0\font-awesome-4.7.0/css/font-awesome.min.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap" rel="stylesheet">
@@ -36,10 +38,10 @@
                     <td><p class="dial-no" id="1">0</p><p class="alphabets"></p></td>
                     <td><p class="dial-no" id="1">#</p><p class="alphabets"></p></td>
                 </tr>
-                <tr class="dial-signs">
+                <tr>
                     <td><p class="dial-no signs"><i class="fa fa-star-o" aria-hidden="true"></i></i></p></td>
-                    <td class="dial1"><p class="dial-no signs"><i class="fa fa-phone" aria-hidden="true"></i></p></td>
-                    <td><p id="dial" class=" dial-no signs"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></p></p></td>
+                    <td class="dial"><p class="dial-no signs"><i class="fa fa-phone" aria-hidden="true"></i></p></td>
+                    <td><p id="del" class=" dial-no signs"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></p></td>
                 </tr>
             </table>
         </div>
